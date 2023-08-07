b8937fdd782671eebfdfbebf47707c4e50436113
rikifadillah <69122888+rikifadillah@users.noreply.github.com>
2023-08-08 04:10:16 +0700

    Add files via upload

---
+++ gis/home.php
@@ -0,0 +1,15 @@
+<div class="page-header">
+    <h1 class="text-black">Sistem Informasi Geografis KIOS</h1>
+    <h1 class="text-black">Pada Pasar Horas Jaya Kota Pematangsiantar</h1>
+</div>
+
+<p class="text-success">Sistem Informasi Geografis (bahasa Inggris: Geographic Information System disingkat GIS) adalah sistem 
+informasi khusus yang mengelola data yang memiliki informasi spasial (bereferensi keruangan). 
+Atau dalam arti yang lebih sempit, adalah sistem komputer yang memiliki kemampuan untuk membangun, 
+menyimpan, mengelola dan menampilkan informasi berefrensi geografis, misalnya data yang diidentifikasi 
+menurut lokasinya, dalam sebuah database. Para praktisi juga memasukkan orang yang membangun dan mengoperasikannya 
+dan data sebagai bagian dari sistem ini.</p>
+<p class="text-success"><strong>Sistem Informasi Geografis Kios Pada Pasar Horas Jaya Kota Pematangsiantar</strong> merupakan sistem
+yang mengelolah tentang informasi geografis pada Pasar Horas Jaya Kota Pematangsiantar. Sistem ini menjelaskan
+tentang penyebaran letak letak Kios pada Pasar Horas Jaya Kota Pematangsiantar, di sertakan dengan galery tentang
+beberapa Kios tersebut.</p>
